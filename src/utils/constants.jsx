2618e83f0f8bb8d1f1c0e330257bbd32f8dff22b
Pratik Manoj Akarte <pratikak3321@gmail.com>
2024-02-23 20:20:14 +0530

    API CHANGES

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -9,4 +9,6 @@ export const API_OPTIONS = {
 
 export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w780";
 
-export const Gemini_API_Key = "AIzaSyD3JAwX5PWC0EDsLcYn6JBOtyMy3O_3iw8";
+// export const Gemini_API_Key = "AIzaSyD3JAwX5PWC0EDsLcYn6JBOtyMy3O_3iw8";
+
+export const Gemini_API_Key = "AIzaSyCHgUFo5MprUCT88iWezXfzPk2Eh_TcLdo";
